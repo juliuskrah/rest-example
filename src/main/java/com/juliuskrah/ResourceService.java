f093c6fa7826716a27c591178e2d38d47387410c
Julius Krah <juliuskrah@gmail.com>
2017-07-16 00:11:51 +0000

    Added LICENSE header to all files

--- src/main/java/com/juliuskrah/ResourceService.java
+++ src/main/java/com/juliuskrah/ResourceService.java
@@ -1,3 +1,18 @@
+/*
+* Copyright 2017, Julius Krah
+* by the @authors tag. See the LICENCE in the distribution for a
+* full listing of individual contributors.
+*
+* Licensed under the GNU General Public License, Version 3 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* http://www.gnu.org/licenses/
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.juliuskrah;
 
 import javax.ws.rs.*;
@@ -5,10 +20,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 @Path("/api/v1.0/resources")
 @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
@@ -49,6 +61,9 @@ public class ResourceService {
 
     @POST
     public Response createResource(Resource resource) {
+        if (Objects.isNull(resource.getId()))
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+
         int index = Collections.binarySearch(resources, resource, Comparator.comparing(Resource::getId));
 
         if (index < 0) {
@@ -60,5 +75,40 @@ public class ResourceService {
                     .build();
         } else
             throw new WebApplicationException(Response.Status.CONFLICT);
+    }
+
+
+    @PUT
+    @Path("{id: [0-9]+}")
+    public Response updateResource(@PathParam("id") Long id, Resource resource) {
+        resource.setId(id);
+        int index = Collections.binarySearch(resources, resource, Comparator.comparing(Resource::getId));
+
+        if (index >= 0) {
+            Resource updatedResource = resources.get(index);
+            updatedResource.setModifiedTime(LocalDateTime.now());
+            updatedResource.setDescription(resource.getDescription());
+            resources.set(index, updatedResource);
+            return Response
+                    .status(Response.Status.NO_CONTENT)
+                    .build();
+        } else
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+    }
+
+    @DELETE
+    @Path("{id: [0-9]+}")
+    public Response deleteResource(@PathParam("id") Long id) {
+        Resource resource = new Resource(id, null, null, null);
+        int index = Collections.binarySearch(resources, resource, Comparator.comparing(Resource::getId));
+
+        if (index >= 0) {
+            resources.remove(index);
+            return Response
+                    .status(Response.Status.NO_CONTENT)
+                    .build();
+
+        } else
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
     }
 }
