183c2d17ec3d4013236aedede32cd8430c324ef0
Julius Krah <juliuskrah@gmail.com>
2017-07-19 22:33:59 +0000

    Added `/api/v1.0/resource/` mapping

--- src/main/java/com/juliuskrah/ResourceService.java
+++ src/main/java/com/juliuskrah/ResourceService.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1.0/resources")
 public class ResourceService {
 
+    @GetMapping(produces = { "text/plain" })
+    String hello() {
+        return "Hello World!";
+    }
+
     @GetMapping("{first}/{last}")
     String hello(@PathVariable Map<String, String> pathMap) {
         return String.format("Hello %s %s!", pathMap.get("first"), pathMap.get("last"));
