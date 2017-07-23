99cd689d55c086860a06bd21c36dedbb07b06137
Julius Krah <juliuskrah@gmail.com>
2017-07-23 22:19:49 +0000

    Completed all tests

---
+++ src/test/java/com/juliuskrah/TestUtil.java
@@ -0,0 +1,19 @@
+package com.juliuskrah;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class TestUtil {
+	public static byte[] convertObjectToJsonBytes(Object object) throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+		JavaTimeModule module = new JavaTimeModule();
+		mapper.registerModule(module);
+
+		return mapper.writeValueAsBytes(object);
+	}
+}
