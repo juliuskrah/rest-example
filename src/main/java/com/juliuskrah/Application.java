4524816fd754d79627bb621ef7a4ff58895546fb
Julius Krah <juliuskrah@gmail.com>
2017-07-24 19:38:53 +0000

    Added Jersey configuration

--- src/main/java/com/juliuskrah/Application.java
+++ src/main/java/com/juliuskrah/Application.java
@@ -15,9 +15,6 @@
 */
 package com.juliuskrah;
 
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
-
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -28,11 +25,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public Module jaxbAnnotation() {
-        return new JaxbAnnotationModule();
     }
 
     @Bean
--- src/test/java/com/juliuskrah/ResourceServiceTest.java
+++ src/test/java/com/juliuskrah/ResourceServiceTest.java
@@ -15,92 +15,168 @@
 */
 package com.juliuskrah;
 
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.test.JerseyTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_XML;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import java.net.URI;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertThat;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-public class ResourceServiceTest extends JerseyTest {
-	@Override
-    protected Application configure() {
-		ResourceConfig rc = new ResourceConfig();
-		ConfigurableApplicationContext ctx = SpringApplication.run(com.juliuskrah.Application.class);
-  		rc.property("contextConfig", ctx);
-        return rc;
-    }
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureMockMvc
+public class ResourceServiceTest {
+	@Autowired
+	private TestRestTemplate restTemplate;
+	@Autowired
+	private MockMvc restMvc;
 
-    @Test
-    public void testGetResources() {
-        List<Resource> resources = target("/api/v1.0/resources/")
-                .request().get(new GenericType<List<Resource>>() {
-                });
-        assertThat(resources, is(not(empty())));
-        assertThat(resources.stream().findFirst().get().getDescription(), is("Resource One"));
-        assertThat(resources, hasSize(10));
-    }
+	@Test
+	public void testGetResources() {
+		ResponseEntity<List<Resource>> response = this.restTemplate.exchange("/api/v1.0/resources", GET, null,
+				new ParameterizedTypeReference<List<Resource>>() {
+				});
 
-    @Test
-    public void testGetResource() {
-        Response response = target("/api/v1.0/resources/")
-                .path("{id}")
-                .resolveTemplate("id", 4L)
-                .request(MediaType.APPLICATION_XML_TYPE)
-                .get();
+		assertThat(response.getBody()).isNotNull();
+		List<Resource> resources = response.getBody();
 
-        assertThat(response, is(notNullValue()));
-        assertThat(response.getMediaType().toString(), is(equalTo(MediaType.APPLICATION_XML)));
-        assertThat(response.getStatus(), is(equalTo(200)));
-    }
+		assertThat(resources).isNotEmpty();
+		assertThat(response.getStatusCode()).isSameAs(OK);
+		assertThat(resources).hasOnlyElementsOfType(Resource.class);
+		// assertThat(resources).asList().isSorted();
+	}
 
-    @Test
-    public void testCreateResource() {
-        Resource resource = new Resource(87L, "Resource Eighty-Seven", LocalDateTime.now(), null);
-        Response response = target("/api/v1.0/resources/")
-                .request(MediaType.APPLICATION_XML_TYPE)
-                .post(Entity.xml(resource));
-        assertThat(response, is(notNullValue()));
-        assertThat(response.getStatus(), is(equalTo(201)));
-        assertThat(response.getLocation(), is(equalTo(URI.create(String
-                .format("http://localhost:9998/api/v1.0/resources/%s", resource.getId())))));
-    }
+	@Test
+	public void testGetResourcesMock() throws Exception {
+		// @formatter:off
+		this.restMvc.perform(get("/api/v1.0/resources/")
+				.accept(APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].id").value(1))
+				.andExpect(jsonPath("$[*].id", hasItems(1, 3, 8)))
+				.andExpect(jsonPath("$[*].description", hasItems("Resource Three", "Resource Four")));
+		// @formatter:on
+	}
 
-    @Test
-    public void testUpdateResource() {
-        Resource resource = new Resource(7L, "Resource Seven modified", LocalDateTime.now(), null);
-        Response response = target("/api/v1.0/resources/")
-                .path("{id}")
-                .resolveTemplate("id", resource.getId())
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .put(Entity.json(resource));
-        assertThat(response, is(notNullValue()));
-        assertThat(response.getStatus(), is(equalTo(204)));
-    }
+	@Test
+	public void testGetResourceIsFound() {
+		Resource resource = this.restTemplate.getForObject("/api/v1.0/resources/{id}", Resource.class, 2L);
 
-    @Test
-    public void testDeleteResource() {
-        Response response = target("/api/v1.0/resources/")
-                .path("{id}")
-                .resolveTemplate("id", 8L)
-                .request()
-                .delete();
-        assertThat(response, is(notNullValue()));
-        assertThat(response.getStatus(), is(equalTo(204)));
-    }
+		assertThat(resource).isNotNull();
+		assertThat(resource.getDescription()).isEqualTo("Resource Two");
+	}
+
+	@Test
+	public void testGetResourceIsNotFound() throws Exception {
+		// @formatter:off
+		this.restMvc.perform(get("/api/v1.0/resources/{id}", 11L)
+				.accept(APPLICATION_XML))
+				.andExpect(status().isNotFound());
+		// @formatter:on
+	}
+
+	@Test
+	public void testCreateResourceIsConflict() {
+		Resource resource = new Resource(2L, "Reource Two Conflict", LocalDateTime.now(), null);
+		HttpEntity<Resource> entity = new HttpEntity<>(resource);
+		ResponseEntity<Void> response = this.restTemplate.exchange("/api/v1.0/resources", POST, entity, Void.class);
+
+		assertThat(response).isNotNull();
+		assertThat(response.getStatusCode()).isSameAs(CONFLICT);
+	}
+
+	@Test
+	public void testCreateResourceIsBadRequest() {
+		Resource resource = new Resource(null, "Reource Two Conflict", LocalDateTime.now(), null);
+		HttpEntity<Resource> entity = new HttpEntity<>(resource);
+		ResponseEntity<Void> response = this.restTemplate.exchange("/api/v1.0/resources", POST, entity, Void.class);
+
+		assertThat(response).isNotNull();
+		assertThat(response.getStatusCode()).isSameAs(BAD_REQUEST);
+	}
+
+	@Test
+	public void testCreateResourceIsCreated() throws IOException, Exception {
+		// @formatter:off
+		Resource resource = new Resource(87L, "Reource Eighty-Seven", LocalDateTime.now(), null);
+
+		this.restMvc.perform(post("/api/v1.0/resources")
+				.contentType(APPLICATION_JSON)
+				.content(TestUtil.convertObjectToJsonBytes(resource)))
+				.andExpect(status().isCreated())
+				.andExpect(header().string("location", containsString(String.format("/api/v1.0/resources/%s", resource.getId()))));
+		// @formatter:on
+	}
+
+	@Test
+	public void testUpdateResourceIsNoContent() throws IOException, Exception {
+		// @formatter:off
+		Resource resource = new Resource(null, "Reource Three Updated", LocalDateTime.now(), null);
+		
+		this.restMvc.perform(put("/api/v1.0/resources/{id}", 3L)
+				.accept(APPLICATION_JSON)
+				.contentType(APPLICATION_JSON)
+				.content(TestUtil.convertObjectToJsonBytes(resource)))
+				.andExpect(status().isNoContent());	
+		// @formatter:off
+	}
+
+	@Test
+	public void testUpdateResourceIsNotFound() {
+		Resource resource = new Resource(null, "Reource Twenty Updated", LocalDateTime.now(), null);
+		HttpEntity<Resource> entity = new HttpEntity<>(resource);
+		ResponseEntity<Void> response = this.restTemplate.exchange("/api/v1.0/resources{id}", PUT, entity, Void.class, 20L);
+
+		assertThat(response).isNotNull();
+		assertThat(response.getStatusCode()).isSameAs(NOT_FOUND);
+	}
+	
+	@Test 
+	public void testDeleteResourceIsNotFound() throws Exception {
+		// @formatter:off				
+		this.restMvc.perform(delete("/api/v1.0/resources/{id}", 34L)
+				.accept(APPLICATION_JSON))
+				.andExpect(status().isNotFound());
+		// @formatter:off	
+	}
+	
+	@Test 
+	public void testDeleteResourceIsNoContent() {
+		ResponseEntity<Void> response = this.restTemplate.exchange("/api/v1.0/resources/{id}", DELETE, null, Void.class, 9L);
+
+		assertThat(response).isNotNull();
+		assertThat(response.getStatusCode()).isSameAs(NO_CONTENT);
+	}
 }
